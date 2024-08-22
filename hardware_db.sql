52b39899c147f8486d80408ca99d5587e546a98e
cruz-oss <alisson.giova@gmail.com>
2024-08-22 17:17:26 -0300

    comando pra criar banco e tabela no mysql workbench

---
+++ hardware_db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE hardware_info;
+USE hardware_info;
+
+CREATE TABLE hardware_upgrade (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cpu VARCHAR(255),
+    memory VARCHAR(255),
+    disk VARCHAR(255),
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Fazendo a busca na tabela
+select * from hardware_upgrade;
+ 
